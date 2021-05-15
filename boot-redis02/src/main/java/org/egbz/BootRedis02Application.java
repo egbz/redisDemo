4ac320423f5356c9352448e0903a05bbbd44e9a1
egbz <1023271893@qq.com>
2021-05-15 17:44:23 +0800

    amend

---
+++ boot-redis02/src/main/java/org/egbz/BootRedis02Application.java
@@ -0,0 +1,15 @@
+package org.egbz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author egbz
+ * @date 2021/5/15
+ */
+@SpringBootApplication
+public class BootRedis02Application {
+    public static void main(String[] args) {
+        SpringApplication.run(BootRedis02Application.class, args);
+    }
+}
