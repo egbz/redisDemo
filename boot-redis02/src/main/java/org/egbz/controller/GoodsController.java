208338f9cbca9f38457aa1d1f05f07bad0ff812a
egbz <1023271893@qq.com>
2021-05-16 11:37:57 +0800

    amend

--- boot-redis02/src/main/java/org/egbz/controller/GoodsController.java
+++ boot-redis02/src/main/java/org/egbz/controller/GoodsController.java
@@ -6,12 +6,16 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 /**
  * @author egbz
  * @date 2021/5/15
  */
 @RestController
 public class GoodsController {
+    public static final String REDIS_LOCK = "sellLock";
+
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
@@ -20,19 +24,28 @@ public class GoodsController {
 
     @GetMapping("/buyGoods")
     public String buyGoods() {
-        // get key     看库存数量够不够
-        String res = stringRedisTemplate.opsForValue().get("goods:001");
-        int goodsNumber = res == null ? 0 : Integer.parseInt(res);
+        String value = UUID.randomUUID() + Thread.currentThread().getName();
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value); //setNX
 
-        if (goodsNumber > 0) {
-            int remaining = goodsNumber -1;
-            stringRedisTemplate.opsForValue().set("goods:001", String.valueOf(remaining));
-            System.out.println("成功买到商品, 库存剩余: " + remaining + "      serverPort: " + serverPort);
-            return "成功买到商品, 库存剩余: " + remaining + "      serverPort: " + serverPort;
+        if (flag) {
+            // get key     看库存数量够不够
+            String res = stringRedisTemplate.opsForValue().get("goods:001");
+            int goodsNumber = res == null ? 0 : Integer.parseInt(res);
+
+            if (goodsNumber > 0) {
+                int remaining = goodsNumber - 1;
+                stringRedisTemplate.opsForValue().set("goods:001", String.valueOf(remaining));
+                System.out.println("成功买到商品, 库存剩余: " + remaining + "      serverPort: " + serverPort);
+                stringRedisTemplate.delete(REDIS_LOCK);
+                return "成功买到商品, 库存剩余: " + remaining + "      serverPort: " + serverPort;
+            }
+            System.out.println("------------------- [failed]");
+            stringRedisTemplate.delete(REDIS_LOCK);
+            return "failed";
+        } else {
+            System.out.println(serverPort + "抢锁失败" );
+            return "抢锁失败";
         }
-        System.out.println("------------------- [failed]");
-        return "failed";
-
     }
 
 }
